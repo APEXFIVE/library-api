53ad0435035651792fd172431145d4327f4e33e9
Wisdom Kwawuvi <wisdomkwawuvi23@gmail.com>
2024-10-08 13:17:59 +0000

    correction made

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,11 @@ import { logIn, logOut, register } from "../controllers/users.js";
 const userRouter = Router();
 
 // Define routes
-userRouter.post ('./users/register', register);
+userRouter.post ('/users/register', register);
 
-userRouter.post ('./users/login', logIn);
+userRouter.post ('/users/login', logIn);
 
-userRouter.post ('./users/logout', logOut);
+userRouter.post ('/users/logout', logOut);
 
 // export  router 
 export default userRouter;
